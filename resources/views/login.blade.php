45c8942a324106ba04e649e132b35698182db9ff
Anuraaaa <anwarfauzan75@gmail.com>
2022-11-19 16:41:30 +0700

    add partial (navbar, notify, slider)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,37 +11,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-        <div class="container">
-            <img src="img/tokopedialogo.png" alt="" class="imgbrand">
-            <a class="navbar-brand" href="{{ url('home') }}">Tokopaedi</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ url('home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link active" href="{{ url('login') }}">Login</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>    
+    @include('partials.navbar')
     <form action="/actionlogin" method="post">
         @csrf
         <div class="container mt-4">
             <h1>Login</h1>
             <p>Please fill in this form to sign in account.</p>
             <hr>
-            @if(session('success'))
-                <p class="alert alert-success">{{ session('success') }}</p>
-            @endif
-            @if (session('error'))
-                <p class="alert alert-danger">{{ session('error') }}</p>
-            @endif
+            @include('partials.notify')
             <label for="email"><b>Email</b></label>
             <input type="text" placeholder="Enter Email" name="email" id="email" required>
             <label for="psw"><b>Password</b></label>
