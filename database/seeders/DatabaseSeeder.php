d8afb5981f1270a94a4078ad3d3a05d7214a68af
Anuraaaa <anwarfauzan75@gmail.com>
2022-11-20 13:18:20 +0700

    add seeder data product (2 data kategori sepatu)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::factory()->create([
+            'nama' => 'Sneaker Hitam Sweet Child',
+            'harga' => 'RP 164.000,00',
+            'deskripsi' => 'nyaman dipake, bisa membuat anda ganteng maksimal',
+            'kategori' => 'sepatu',
+            'image' => 'img/product/sepatu/sepatu.jpg',
+            'updated_at' => now(),
+            'created_at' => now()
+        ]);
+        Product::factory()->create([
+            'nama' => 'Sneaker Putih Long Child',
+            'harga' => 'RP 279.000,00',
+            'deskripsi' => 'nyaman dipake, pernah dipake ronaldowati',
+            'kategori' => 'sepatu',
+            'image' => 'img/product/sepatu/sepatu2.jfif',
+            'updated_at' => now(),
+            'created_at' => now()
+        ]);
     }
 }
