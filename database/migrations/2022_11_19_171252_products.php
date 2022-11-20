0884de8d0388ef140cb5bd50f595d4e85b1c199a
Anuraaaa <anwarfauzan75@gmail.com>
2022-11-20 13:17:35 +0700

    add data product migration

---
+++ database/migrations/2022_11_19_171252_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 64);
+            $table->string('harga');
+            $table->string('deskripsi', 96);
+            $table->string('kategori');
+            $table->string('image', 96);
+            $table->string('updated_at');
+            $table->string('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
